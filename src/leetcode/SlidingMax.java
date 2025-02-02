be247f0ba1134f6fbab9a8051fd9ed4ab960a31d
ruslanmerdeev <r.merdeev@haulmont.com>
2025-02-02 10:32:32 +0300

    [main] Added the Leetcode tasks

--- src/leetcode/SlidingMax.java
+++ src/leetcode/SlidingMax.java
@@ -20,33 +20,68 @@ public class SlidingMax {
             }
 
             // window
-            int[] window;// = Arrays.copyOfRange(nums, 0, k);
+            int[] window = Arrays.copyOfRange(nums, 0, k);
+            int max = Arrays.stream(window).max().getAsInt();
 
             // result
             int resultN = n - k + 1;
             int[] result = new int[resultN];
+            result[0] = max;
             int pos = 0;
 
+//            max = updateWindow(window, nums[0], nums[k], max);
+
             // computation
-            do {
-                window = Arrays.copyOfRange(nums, pos, pos+k);//updateWindow(window, nums[pos], nums[pos+k]);
-                result[pos] = Arrays.stream(window).max().getAsInt();
+            // [1,-4,3,6,1,7], 3 -> [3,6,6,7]
+            // pos = [0,\1,2,3\]
+            while(pos < resultN - 1) {
+//                result[pos] = findMax(window);//Arrays.stream(window).max().getAsInt();
+//                if (pos < resultN - 1) {
+                    result[pos+1] = updateWindow(window, nums[pos], nums[pos+k], max);
+//                }
                 pos++;
-            } while(pos < resultN);
+            }
 
             return result;
         }
 
-//        updateWindow(int[] window, int toRemove, int toAdd) {
-//
-//        }
+        int updateWindow(int[] window, int toRemove, int toAdd, int prevMax) {
+            if (toRemove != toAdd) {
+                int len = window.length;
+                for (int i = 0; i < len; i++) {
+                    if (window[i] == toRemove) {
+                        window[i] = toAdd;
+                        if (toAdd > prevMax) {
+                            return toAdd;
+                        } else {
+                            return findMax(window);
+                        }
+                    }
+                }
+            }
+            return prevMax;
+        }
+
+        int findMax(int[] window) {
+            int max = Integer.MIN_VALUE;
+            for (int num : window) {
+                if (num > max) {
+                    max = num;
+                }
+            }
+            return max;
+        }
     }
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         Solution solution = new SlidingMax().new Solution();
-        int[] nums = new int[]{2147483647,1,2,3,4,5,6,7,2147483647};
-        int target = 2;
+//        int[] nums = new int[]{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int[] nums = new int[]{1,3,-1,-3,5,3,6,7};
+        int target = 3;//1000;
         int[] result = solution.maxSlidingWindow(nums, target);
-        System.out.println(Arrays.toString(result));
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println(Arrays.toString(result) + " " + timeElapsed);
     }
 }
