501297dee917ffb49bba333c8432723cac2e9679
ruslanmerdeev <r.merdeev@haulmont.com>
2026-03-03 14:45:23 +0400

    leetcode

---
+++ src/leetcode/MinimumDepthOfBinaryTree.kt
@@ -0,0 +1,40 @@
+package leetcode
+
+import kotlin.math.min
+
+class MinimumDepthOfBinaryTree {
+    fun minDepth(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1
+        }
+
+        if (root.left == null) {
+            return minDepth(root.right)+1
+        }
+
+        if (root.right == null) {
+            return minDepth(root.left)+1
+        }
+
+        return min(minDepth(root.left), minDepth(root.right))+1
+    }
+}
+
+fun main() {
+    val solution = MinimumDepthOfBinaryTree()
+
+    check(solution.minDepth(null) == 0)
+    check(solution.minDepth(TreeNode(0)) == 1)
+    check(solution.minDepth(TreeNode(0, TreeNode(-1))) == 2)
+    check(solution.minDepth(TreeNode(0, TreeNode(-1), TreeNode(1))) == 2)
+    check(solution.minDepth(TreeNode(0, TreeNode(-1, TreeNode(-2)), TreeNode(1))) == 2)
+    check(solution.minDepth(TreeNode(0, TreeNode(-1, TreeNode(-2, TreeNode(-3, TreeNode(-4)))))) == 5)
+
+    //[2,null,3,null,4,null,5,null,6] == 5
+
+    println("ok")
+}
