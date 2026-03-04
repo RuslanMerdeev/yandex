2a3f9583a4a158a0efd8c304ebd953c75969772d
ruslanmerdeev <r.merdeev@haulmont.com>
2026-03-04 16:14:47 +0400

    leetcode

---
+++ src/leetcode/PathSum.kt
@@ -0,0 +1,35 @@
+package leetcode
+
+class PathSum {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        if (root == null) {
+            return false
+        }
+
+        if (root.left == null && root.right == null) {
+            return root.`val` == targetSum
+        }
+
+        if (root.left != null) {
+            if (hasPathSum(root.left, targetSum-root.`val`)) {
+                return true
+            }
+        }
+
+        return hasPathSum(root.right, targetSum-root.`val`)
+    }
+}
+
+fun main() {
+    val solution = PathSum()
+
+    check(!solution.hasPathSum(null, 0))
+    check(solution.hasPathSum(TreeNode(0), 0))
+    check(!solution.hasPathSum(TreeNode(0, TreeNode(2), TreeNode(3)), 0))
+    check(solution.hasPathSum(TreeNode(0, TreeNode(2), TreeNode(3)), 2))
+    check(solution.hasPathSum(TreeNode(0, TreeNode(2, TreeNode(4)), TreeNode(3)), 6))
+    check(solution.hasPathSum(TreeNode(0, TreeNode(2, TreeNode(4)), TreeNode(3)), 3))
+    check(!solution.hasPathSum(TreeNode(0, TreeNode(2, TreeNode(4), TreeNode(5)), TreeNode(3)), 8))
+
+    println("ok")
+}
